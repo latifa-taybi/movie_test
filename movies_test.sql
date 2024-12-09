2d09323bb53a68eb43603234d2abfca94d0d04e1
latifa <taybilatifa46@gmail.com>
2024-12-09 16:43:23 +0100

    first commit

---
+++ movies_test.sql
@@ -0,0 +1,61 @@
+-- nom : LATIFA TAYBI
+-- email : taybilatifa46@gmail.com
+
+-- create database
+CREATE DATABASE movies_test;
+USE movies_test;
+
+-- create table subscription
+CREATE TABLE subscription( 
+    subscriptionId int AUTO_INCREMENT PRIMARY KEY, 
+    subscriptionType varchar(50) NOT NULL, 
+    CHECK(subscriptionType = 'basic' OR subscriptionType = 'premium'), 
+    monthlyFee decimal(10,2) NOT NULL
+);
+
+-- create table users
+CREATE TABLE users(
+    userId int AUTO_INCREMENT PRIMARY KEY,
+    firstName varchar(100) NOT NULL,
+    lastName varchar(100) NOT NULL,
+    email varchar(100) UNIQUE,
+    registrationDate date NOT NULL,
+    subscriptionId int,
+    FOREIGN KEY (subscriptionId) REFERENCES subscription(subscriptionId)
+);
+
+-- create table movie
+CREATE TABLE movie( 
+    movieId int AUTO_INCREMENT PRIMARY KEY, 
+    title varchar(255) NOT NULL UNIQUE, 
+    genre varchar(255) NOT NULL, 
+    releaseYear int NOT NULL, 
+    duration int NOT NULL, 
+    rating varchar(10) NOT NULL 
+);
+
+-- create table watchhistory
+CREATE TABLE watchhistory( 
+    watchhistoryId int AUTO_INCREMENT PRIMARY KEY, 
+    watchdate date NOT NULL, 
+    completionpercentage int DEFAULT 0, 
+    userId int, 
+    movieId int, 
+    FOREIGN KEY (userId) REFERENCES users(userId), 
+    FOREIGN KEY (movieId) REFERENCES movie(movieId)
+);
+
+-- create table review
+CREATE TABLE review(
+    reviewId int AUTO_INCREMENT PRIMARY KEY,
+    rating int NOT NULL,
+    reviewText text,
+    reviewDate date NOT NULL,
+    userId int, 
+    movieId int, 
+    FOREIGN KEY (userId) REFERENCES users(userId), 
+    FOREIGN KEY (movieId) REFERENCES movie(movieId)
+);
+-- Insérer un film : Ajouter un nouveau film intitulé Data Science Adventures dans le genre "Documentary".
+INSERT INTO movie(title, genre, releaseYear, duration, rating) 
+VALUES ('hello','documentary','2002','30','kk');
